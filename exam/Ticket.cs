57b5281fb0a76fde6da3bc07776b0c774af82926
OleksandrKeshel-STUD <OleksandrKeshel-STUD@S-DQ1H504-SL14>
2025-01-08 13:20:11 +0000

    q1 c,d,e done - created class Ticket and VIPTicket

---
+++ exam/Ticket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    public class Ticket
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int AvailableTickets { get; set; }
+    }
+}
---
+++ exam/VIPTicket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    public class VIPTicket: Ticket
+    {
+        public string AdditionalExtras { get; set; }
+        public decimal AdditionalCost { get; set; }
+    }
+}
